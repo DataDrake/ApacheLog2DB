a7ddb6e366c31dc37b18451baf46d06c36be8d1b
Bryan T. Meyers <btmeme@rit.edu>
2016-05-31 08:05:44 -0400

    Workspace cleanup

--- src/ApacheLog2DB/log2db.go
+++ src/ApacheLog2DB/log2db.go
@@ -9,9 +9,16 @@ import (
 	"database/sql"
 	"errors"
 	_ "github.com/mattn/go-sqlite3"
+	"regexp"
+	"io"
+"strings"
+	"time"
+	"strconv"
 )
 
 var LOG2DB_TABLES = []string{"destinations", "sources", "transactions", "users", "user_agents"}
+var APACHE_COMBINED = regexp.MustCompile("^(\\S*).(\\S*).(\\S*).\\[(.*)\\].\"([^\"]*)\".(\\d{3}).(\\d*).\"([^\"]*)\".\"([^\"]*)\"$")
+var APACHE_TIME_LAYOUT = "2006-01-02T15:04:05.000Z"
 
 func SliceContains(vs []string, v string) bool {
 	for _, curr := range vs {
@@ -99,4 +106,45 @@ func CheckTables(db *sql.DB) (error, []string) {
 		return errors.New("Missing tables"), missing
 	}
 	return nil, missing
+}
+
+func ImportLog(log string) error{
+	for _,line := range APACHE_COMBINED.FindAllStringSubmatch(log,-1) {
+		source := line[1]
+		ident := line[2]
+		username := line[3]
+		occurred,err := time.Parse(APACHE_TIME_LAYOUT,line[4])
+		if err != nil {return err}
+		request := strings.Split(line[5], " ")
+
+		var verb string
+		var uri string
+		var protocol string
+		switch len(request) {
+		case 3:
+			verb = request[0]
+			uri = request[1]
+			protocol = request[2]
+		case 2:
+			uri = request[0]
+			protocol = request[1]
+		case 1:
+			uri = request[0]
+		}
+
+		status,err := strconv.Atoi(line[6])
+		if err != nil {return err}
+
+		size,err := strconv.Atoi(line[7])
+		if err != nil {return err}
+
+		referrer := line[8]
+
+		agentname := line[9]
+
+	}
+}
+
+func ExportLog(w *io.Writer) {
+
 }
