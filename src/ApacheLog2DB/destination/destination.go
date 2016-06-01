d2a88a0823ac708443bd17d664cb98260bc41394
Bryan T. Meyers <btmeme@rit.edu>
2016-06-01 09:06:26 -0400

    Simplified source

--- src/ApacheLog2DB/destination/destination.go
+++ src/ApacheLog2DB/destination/destination.go
@@ -35,62 +35,41 @@ func Insert(db *sql.DB, d *Destination) error {
 }
 
 func ReadURI(db *sql.DB, uri string) (*Destination, error) {
-	var d *Destination
+	d := &Destination{}
 	var err error
 	row := db.QueryRow("SELECT * FROM destinations WHERE uri=?", uri)
 	if row == nil {
 		err = errors.New("Destination not found")
 	} else {
-		var id int
-		var uri string
-		err = row.Scan(&id, &uri)
-		if err == nil {
-			d = &Destination{id, uri}
-		}
+		err = row.Scan(&d.ID, &d.URI)
 	}
 	return d, err
 }
 
 func Read(db *sql.DB, id int) (*Destination, error) {
-	var d *Destination
+	d := &Destination{}
 	var err error
 	row := db.QueryRow("SELECT * FROM destinations WHERE id=?", id)
 	if row == nil {
 		err = errors.New("Destination not found")
 	} else {
-		var id int
-		var uri string
-		err = row.Scan(&id, &uri)
-		if err == nil {
-			d = &Destination{id, uri}
-		}
+		err = row.Scan(&d.ID, &d.URI)
 	}
 	return d, err
 }
 
 func ReadAll(d *sql.DB) ([]*Destination, error) {
 	ds := make([]*Destination, 0)
-	tx, err := d.Begin()
-	if err == nil {
-		rows, err := tx.Query("SELECT * FROM destinations")
-		if err != nil {
-			tx.Rollback()
-		} else {
-			var id int
-			var uri string
-			rows.Scan(&id, &uri)
-			if id >= 0 && len(uri) > 0 {
-				ds = append(ds, &Destination{id, uri})
+	rows, err := d.Query("SELECT * FROM destinations")
+	if err != nil {
+		for rows.Next() {
+			d := &Destination{}
+			rows.Scan(&d.ID, &d.URI)
+			if d.ID >= 0 && len(d.URI) > 0 {
+				ds = append(ds, d)
 			}
-			for rows.Next() {
-				rows.Scan(&id, &uri)
-				if id >= 0 && len(uri) > 0 {
-					ds = append(ds, &Destination{id, uri})
-				}
-			}
-			rows.Close()
-			tx.Commit()
 		}
+		rows.Close()
 	}
 	return ds, err
 }
